3cde53fa33624fc401b4d36f5be2784dbb50377d
Julian <portalsoup@gmail.com>
2021-01-21 18:01:53 -0800

    Cleanup

--- src/jvmMain/kotlin/com/portalsoup/example/fullstack/api/CounterApi.kt
+++ src/jvmMain/kotlin/com/portalsoup/example/fullstack/api/CounterApi.kt
@@ -2,7 +2,8 @@ package com.portalsoup.example.fullstack.api
 
 import com.portalsoup.example.fullstack.business.CounterService
 import com.portalsoup.example.fullstack.common.resources.CounterResource
-import com.portalsoup.example.fullstack.utils.Try
+import com.portalsoup.example.fullstack.utils.Try.Failure
+import com.portalsoup.example.fullstack.utils.Try.Success
 import com.portalsoup.example.fullstack.utils.getLogger
 import io.ktor.application.call
 import io.ktor.response.respond
@@ -19,8 +20,8 @@ object CounterApi {
             get("{name}") {
                 val name = call.parameters["name"] ?: throw RuntimeException("No valid name found.")
                 when (val countForName = CounterService.recordForName(name)) {
-                    is Try.Success -> call.respond(CounterResource(name, countForName.data))
-                    is Try.Failure -> call.respond(500)
+                    is Success -> call.respond(CounterResource(name, countForName.data))
+                    is Failure -> call.respond(500)
                 }
             }
         }
